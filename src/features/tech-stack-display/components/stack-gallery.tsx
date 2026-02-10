5bf7aa673947e58995841245a7fcf51e9d2bac13
juancholopes <177896874+juancholopes@users.noreply.github.com>
2026-02-09 21:44:32 -0500

    feat(styles): fix backagraund in primary buttom component - fix responsive on skill section

--- src/features/tech-stack-display/components/stack-gallery.tsx
+++ src/features/tech-stack-display/components/stack-gallery.tsx
@@ -95,36 +95,38 @@ const StackGallery = () => {
 
   return (
     <TooltipProvider delayDuration={0}>
-      <div className="grid grid-cols-7 gap-4 group">
-        {techStackImages.map((image, index) => (
-          <Tooltip key={index}>
-            <TooltipTrigger asChild>
-              <div className="w-full h-24 flex items-center justify-center bg-gray-50 dark:bg-gray-800/50 p-4 rounded-lg border border-gray-200 dark:border-gray-700 hover:border-blue-300 dark:hover:border-blue-600 transition-all duration-500 ease-in-out cursor-pointer group-hover:opacity-40 group-hover:grayscale hover:!grayscale-0 hover:!opacity-100 hover:scale-110">
-                <img
-                  src={image.src}
-                  alt={image.alt}
-                  className="w-full h-full object-contain"
-                />
-              </div>
-            </TooltipTrigger>
-            <TooltipContent className="w-64 p-4 bg-slate-900 border-slate-800 text-white">
-              <div className="space-y-3">
-                <p className="text-center font-semibold text-base">
-                  {image.name}
-                </p>
-                <div className="flex items-center justify-between gap-4 text-sm text-gray-400">
-                  <span>Experiencia</span>
-                  <div className="h-2 flex-1 bg-gray-700 rounded-full overflow-hidden">
-                    <div
-                      className="h-full bg-blue-500 rounded-full"
-                      style={{ width: `${image.level * 10}%` }}
-                    />
+      <div className="grid place-items-center h-full">
+        <div className="grid gap-4 group grid-cols-2 lg:grid-cols-7  md:grid-cols-4 sm:grid-cols-2">
+          {techStackImages.map((image, index) => (
+            <Tooltip key={index}>
+              <TooltipTrigger asChild>
+                <div className=" w-32 h-16 lg:h-24 lg:w-full flex items-center justify-center bg-gray-50 dark:bg-gray-800/50 p-4 rounded-lg border border-gray-200 dark:border-gray-700 hover:border-blue-300 dark:hover:border-blue-600 transition-all duration-500 ease-in-out cursor-pointer group-hover:opacity-40 group-hover:grayscale hover:!grayscale-0 hover:!opacity-100 hover:scale-110">
+                  <img
+                    src={image.src}
+                    alt={image.alt}
+                    className="w-full h-full object-contain"
+                  />
+                </div>
+              </TooltipTrigger>
+              <TooltipContent className="w-64 p-4 bg-slate-900 border-slate-800 text-white">
+                <div className="space-y-3">
+                  <p className="text-center font-semibold text-base">
+                    {image.name}
+                  </p>
+                  <div className="flex items-center justify-between gap-4 text-sm text-gray-400">
+                    <span>Experiencia</span>
+                    <div className="h-2 flex-1 bg-gray-700 rounded-full overflow-hidden">
+                      <div
+                        className="h-full bg-blue-500 rounded-full"
+                        style={{ width: `${image.level * 10}%` }}
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>
-            </TooltipContent>
-          </Tooltip>
-        ))}
+              </TooltipContent>
+            </Tooltip>
+          ))}
+        </div>
       </div>
     </TooltipProvider>
   );
--- src/shared/components/ui/primary-button.tsx
+++ src/shared/components/ui/primary-button.tsx
@@ -23,7 +23,7 @@ function PrimaryButton({
   className = "",
 }: PrimaryButtonProps) {
   const baseClasses =
-    "inline-flex items-center px-4 sm:px-6 lg:px-8 py-2 sm:py-3 border border-blue-500 text-blue-500 dark:text-blue-400 hover:bg-blue-500 hover:!text-[--zed-light] transition-all duration-300 rounded-lg text-sm sm:text-base";
+    "inline-flex items-center px-4 sm:px-6 lg:px-8 py-2 sm:py-3 border border-blue-500 text-blue-500 dark:[--card-bg] hover:bg-[card-bg] hover:!text-[--zed-light] transition-all duration-300 rounded-lg text-sm sm:text-base";
 
   const combinedClasses = `${baseClasses} ${className}`;
 
